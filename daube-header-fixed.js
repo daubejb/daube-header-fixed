9e71e10ad02c152c7924004d4a461bf09dc17fc3
Jeffrey B. Daube <daubejb@gmail.com>
2017-09-05 13:38:16 -0400

    implement static header with textual title and a slot for anything else

--- daube-header-fixed.js
+++ daube-header-fixed.js
@@ -14,8 +14,30 @@ template.innerHTML = `
       display: none;
     }
 
+    header {
+      display: inline-block;
+      width: 100%;
+      position: fixed;
+      top: 0;
+      background-color: #237d32;
+      color: rgba(255,255,255,0.81);
+      height: 3.5rem;
+      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    }
+
+    .daubemaintitle {
+      padding: 0.92rem 0rem 0.92rem 1.5rem;
+      font-weight: 200;
+      font-size: 1.66rem;
+      height: 3.5rem;
+    }
+
   </style>
-`;
+  <header>
+    <div class="daubemaintitle">Header</div>
+    <slot></slot>
+  </header>
+  `
 
 if (window.ShadyCSS) {
   ShadyCSS.prepareTemplate(template, 'daube-header-fixed');
